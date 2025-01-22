371bac2a14834847277396b35b852c2b2b3ed8d3
Rineta2 <rzkir571@gmail.com>
2025-01-22 11:20:42 +0700

    [update] next.config

--- next.config.ts
+++ next.config.ts
@@ -1,41 +1,13 @@
 /** @type {import('next').NextConfig} */
-
-import type { NextConfig } from "next";
-import bundleAnalyzer from "@next/bundle-analyzer";
-
-interface PathData {
-  chunk: {
-    name?: string;
-  };
-}
+import type { NextConfig } from 'next'
+import bundleAnalyzer from '@next/bundle-analyzer'
 
 const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-});
+  enabled: process.env.ANALYZE === 'true',
+})
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
-  async rewrites() {
-    return [
-      {
-        source: '/super-admins/:path*',
-        destination: '/:path*',
-      },
-      {
-        source: '/admin/:path*',
-        destination: '/:path*',
-      },
-      {
-        source: '/guru/:path*',
-        destination: '/:path*',
-      },
-      {
-        source: '/siswa/:path*',
-        destination: '/:path*',
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
@@ -48,52 +20,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      const entry = typeof config.entry === 'function'
-        ? config.entry()
-        : config.entry;
+}
 
-      config.entry = async () => {
-        const entries = await entry;
-        return {
-          ...entries,
-          'sw': './public/sw.ts',
-        };
-      };
-
-      if (config.output) {
-        config.output.filename = (pathData: PathData) => {
-          return pathData.chunk.name === 'sw' ? 'sw.js' :
-            (typeof config.output?.filename === 'string'
-              ? config.output.filename
-              : '[name].js');
-        };
-      }
-    }
-    return config;
-  },
-  async headers() {
-    return [
-      {
-        source: '/api/notifications/send',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'POST, OPTIONS',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type',
-          },
-        ],
-      },
-    ];
-  },
-};
-
-export default withBundleAnalyzer(nextConfig);
+export default withBundleAnalyzer(nextConfig)
